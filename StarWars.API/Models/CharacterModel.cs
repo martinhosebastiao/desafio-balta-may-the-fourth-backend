ad9ee559cd09da6efa3707ffd2f4319fe9287ae4
Martinho Sebastião <martinho.sebastiao@outlook.com>
2024-04-27 17:37:39 -0300

    Ajuste dos modelos

--- StarWars.API/Models/CharacterModel.cs
+++ StarWars.API/Models/CharacterModel.cs
@@ -8,6 +8,7 @@ public sealed class CharacterModel
     public int CharacterId { get; set; }
     public string Name { get; set; }
     public string Height { get; set; }
+    public string Weight { get; set; }
     public string Mass { get;  set; }
     public string HairColor { get; set; }
     public string SkinColor { get; set; }
